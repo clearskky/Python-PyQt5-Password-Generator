6097eefb49a8ccbf8d9903150c9257b7f9410b09
clearskky <35397726+clearskky@users.noreply.github.com>
2018-03-01 12:46:32 +0300

    Update pwordGen.py

--- pwordGen.py
+++ pwordGen.py
@@ -13,10 +13,10 @@ def pwordGenn(digit):
                 choice = random.choice(p_choices)
 		
                 if choice in [1,2,3]:
-                        password.append(random.choice(letters))
+                        password.append(random.choice(letters)) # 3/7 chance for each character in a password to be a letter.
                 elif choice in [4,5,6]:
-                        password.append(random.choice(numbers))
+                        password.append(random.choice(numbers)) # 3/7 chance for each character in a password to be a number.
                 else:
-                        password.append(random.choice(characters))
-        result = "".join(password)
+                        password.append(random.choice(characters)) # 1/7 chance for a special character. I did it this way because I didn't like it when there were too many special symbols in a password.
+        result = "".join(password) # This line creates a single string out of all the elements in an iterable.
         return result
